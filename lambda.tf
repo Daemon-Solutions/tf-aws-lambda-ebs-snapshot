5f99799c789ccd135d65a5688326516ebf23c717
Pawel Fiuto <pawel@bashton.com>
2017-05-10 16:58:38 +0200

    Removed s3 bucket from the mix

--- lambda.tf
+++ lambda.tf
@@ -9,30 +9,21 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
 }
 
 ## create lambda package
-data "archive_file" "create_lambda_package" {
+data "archive_file" "lambda_package" {
   type        = "zip"
-  source_dir  = "${path.module}/include/"
-  output_path = "/tmp/${var.envname}-${var.service}-lambda/lambda-${var.lambda_version}.zip"
-}
-
-## upload lambda package to s3
-resource "aws_s3_bucket_object" "upload_lambda_package" {
-  depends_on = ["data.archive_file.create_lambda_package"]
-  bucket     = "${aws_s3_bucket.bucket_for_lambda_package.id}"
-  key        = "lambda-snapshot-${var.lambda_version}.zip"
-  source     = "/tmp/${var.envname}-${var.service}-lambda/lambda-${var.lambda_version}.zip"
+  source_file = "${path.module}/include/lambda.py"
+  output_path = "${path.module}/include/lambda.zip"
 }
 
 ## create lambda function
 resource "aws_lambda_function" "volume_backup" {
-  depends_on        = ["aws_s3_bucket_object.upload_lambda_package", "aws_cloudwatch_log_group.lambda_log_group"]
-  s3_bucket         = "${aws_s3_bucket.bucket_for_lambda_package.id}"
-  s3_key            = "lambda-snapshot-${var.lambda_version}.zip"
-  s3_object_version = "null"
-  function_name     = "${var.lambda_function_name}"
-  role              = "${aws_iam_role.lambda_role.arn}"
-  handler           = "lambda.lambda_handler"
-  runtime           = "python2.7"
-  timeout           = "60"
-  publish           = true
+  depends_on       = ["aws_cloudwatch_log_group.lambda_log_group"]
+  filename         = "${path.module}/include/lambda.zip"
+  source_code_hash = "${data.archive_file.lambda_package.output_base64sha256}"
+  function_name    = "${var.lambda_function_name}"
+  role             = "${aws_iam_role.lambda_role.arn}"
+  handler          = "lambda.lambda_handler"
+  runtime          = "python2.7"
+  timeout          = "60"
+  publish          = true
 }
